015cd0ef71701c62c7bf148ac4f7410f10459de2
Kirill Suvorov <kirill_suvorov1@icloud.com>
2024-08-23 14:03:51 +0300

    Initial Commit

---
+++ ToDoList/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ToDoList
+//
+//  Created by Kirill Suvorov on 23.08.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
